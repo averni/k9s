d8d299b218fe760a6d3c3340d67431dc451edfd6
Antonio Verni <me.verni@gmail.com>
2023-09-27 21:08:09 +0200

    feat(clusterrole): adds new hotkey 'x' to clusterrole view to browse aggregated roles. Adds new columns 'aggr' and 'aggr-to' to wide mode

--- internal/render/cr.go
+++ internal/render/cr.go
@@ -5,9 +5,12 @@ package render
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/derailed/k9s/internal/client"
 	rbacv1 "k8s.io/api/rbac/v1"
+	v1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -21,6 +24,8 @@ type ClusterRole struct {
 func (ClusterRole) Header(string) Header {
 	return Header{
 		HeaderColumn{Name: "NAME"},
+		HeaderColumn{Name: "AGGR", Wide: true},
+		HeaderColumn{Name: "AGGR-TO", Wide: true},
 		HeaderColumn{Name: "LABELS", Wide: true},
 		HeaderColumn{Name: "AGE", Time: true},
 	}
@@ -38,12 +43,40 @@ func (ClusterRole) Render(o interface{}, ns string, r *Row) error {
 		return err
 	}
 
+	aggregated := ""
+	if hasAggregation(&cr) {
+		aggregated = "ⓨ"
+	}
+
 	r.ID = client.FQN("-", cr.ObjectMeta.Name)
 	r.Fields = Fields{
 		cr.Name,
+		aggregated,
+		readAggregateTo(cr.Labels),
 		mapToStr(cr.Labels),
 		toAge(cr.GetCreationTimestamp()),
 	}
 
 	return nil
+}
+
+// helpers
+const aggregateToLabelPrefix = "/aggregate-to-"
+
+func readAggregateTo(labels map[string]string) string {
+	aggregateTo := make([]string, 0, 10)
+	for label := range labels {
+		aggregateToLabelIndex := strings.Index(label, aggregateToLabelPrefix)
+		if aggregateToLabelIndex >= 0 && strings.HasSuffix(labels[label], "true") {
+			aggregateTo = append(aggregateTo, label[aggregateToLabelIndex+len(aggregateToLabelPrefix):])
+		}
+	}
+	if len(aggregateTo) > 0 {
+		sort.Strings(aggregateTo)
+	}
+	return strings.Join(aggregateTo, ",")
+}
+
+func hasAggregation(cr *v1.ClusterRole) bool {
+	return cr.AggregationRule != nil && len(cr.AggregationRule.ClusterRoleSelectors) > 0
 }
---
+++ internal/view/cr.go
@@ -0,0 +1,81 @@
+package view
+
+import (
+	"context"
+
+	"github.com/derailed/k9s/internal"
+	"github.com/derailed/k9s/internal/client"
+	"github.com/derailed/k9s/internal/dao"
+	"github.com/derailed/k9s/internal/ui"
+	"github.com/derailed/tcell/v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ClusterRole represents a PVC custom viewer.
+type ClusterRole struct {
+	ResourceViewer
+}
+
+// NewClusterRole returns a new viewer.
+func NewClusterRole(gvr client.GVR) ResourceViewer {
+	v := ClusterRole{
+		ResourceViewer: NewBrowser(gvr),
+	}
+	v.AddBindKeysFn(v.bindKeys)
+	return &v
+}
+
+func (c *ClusterRole) bindKeys(aa ui.KeyActions) {
+	aa.Add(ui.KeyActions{
+		ui.KeyX: ui.NewKeyAction("Expand Aggregation", c.showAggregation(), true),
+	})
+}
+
+func showClusterRoles(app *App, path string, sel *metav1.LabelSelector) {
+	l, err := metav1.LabelSelectorAsSelector(sel)
+	if err != nil {
+		app.Flash().Err(err)
+		return
+	}
+
+	v := NewClusterRole(client.NewGVR("rbac.authorization.k8s.io/v1/clusterroles"))
+
+	v.SetContextFn(crCtx(path, l.String()))
+
+	if err := app.inject(v, false); err != nil {
+		app.Flash().Err(err)
+	}
+}
+
+func (c *ClusterRole) showAggregation() func(evt *tcell.EventKey) *tcell.EventKey {
+	return func(evt *tcell.EventKey) *tcell.EventKey {
+		path := c.GetTable().GetSelectedItem()
+		if path == "" {
+			return nil
+		}
+
+		var crDao dao.Rbac
+		cr, err := crDao.LoadClusterRole(c.App().factory, path)
+		if err != nil {
+			c.App().Flash().Err(err)
+			return nil
+		}
+		if cr.AggregationRule == nil || len(cr.AggregationRule.ClusterRoleSelectors) == 0 {
+			c.App().Flash().Errf("ClusterRole %s does not have any aggregation rules", path)
+			return nil
+		}
+		// TODO: Support multiple selectors
+		showClusterRoles(c.App(), path, &cr.AggregationRule.ClusterRoleSelectors[0])
+
+		return nil
+	}
+}
+
+func crCtx(path, ls string) ContextFunc {
+	return func(ctx context.Context) context.Context {
+		ctx = context.WithValue(ctx, internal.KeyPath, "")
+		ctx = context.WithValue(ctx, internal.KeySubjectKind, "ClusterRole")
+		ctx = context.WithValue(ctx, internal.KeySubjectName, path)
+		return context.WithValue(ctx, internal.KeyLabels, ls)
+	}
+}

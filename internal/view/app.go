be97393cf6e546d3c41db9a74ec9b27e4f34e5cd
Antonio Verni <me.verni@gmail.com>
2024-10-11 15:01:16 +0200

    feat: Keep current resource type when switching ctx <CONTEXT>

--- internal/view/app.go
+++ internal/view/app.go
@@ -448,6 +448,8 @@ func (a *App) switchContext(ci *cmd.Interpreter, force bool) error {
 	a.Halt()
 	defer a.Resume()
 	{
+		activeView := a.Config.ActiveView()
+		ns := a.Config.ActiveNamespace()
 		a.Config.Reset()
 		ct, err := a.Config.ActivateContext(contextName)
 		if err != nil {
@@ -457,12 +459,11 @@ func (a *App) switchContext(ci *cmd.Interpreter, force bool) error {
 			ct.Namespace.Active = cns
 		}
 
-		p := cmd.NewInterpreter(a.Config.ActiveView())
+		p := cmd.NewInterpreter(activeView)
 		p.ResetContextArg()
 		if p.IsContextCmd() {
 			a.Config.SetActiveView(client.PodGVR.String())
 		}
-		ns := a.Config.ActiveNamespace()
 		if !a.Conn().IsValidNamespace(ns) {
 			slog.Warn("Unable to validate namespace", slogs.Namespace, ns)
 			if err := a.Config.SetActiveNamespace(ns); err != nil {
@@ -486,7 +487,7 @@ func (a *App) switchContext(ci *cmd.Interpreter, force bool) error {
 		)
 		a.Flash().Infof("Switching context to %q::%q", contextName, ns)
 		a.ReloadStyles()
-		a.gotoResource(a.Config.ActiveView(), "", true, true)
+		a.gotoResource(activeView, "", true, true)
 		a.clusterModel.Reset(a.factory)
 	}
 
